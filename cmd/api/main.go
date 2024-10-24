e33e3cdf2ff99e35696aaab0bbd3e79da38d2fc2
arthur <arthur.broudoux@gmail.com>
2024-10-24 12:24:17 +0200

    feat: Create database connection, docker-compose, basic controller Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/abroudoux/go-rest-api-boilerplate/internal/router"
+import (
+	"github.com/abroudoux/go-rest-api-boilerplate/internal/database"
+	"github.com/abroudoux/go-rest-api-boilerplate/internal/router"
+)
 
 func main() {
+	db, err := database.Connect()
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.Migrate(db)
 	router.StartNewRouter()
 }
